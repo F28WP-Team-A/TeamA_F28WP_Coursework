47b4860543e331005f4a29007c006329cdb34596
Elliot Whitehouse <71378523+ElliotW65@users.noreply.github.com>
2020-10-18 16:21:17 +0100

    Updated ninja.js to include collision detection, resized sprite images, additional sprite image and menu initiation plus fade out

--- ninja/ninja.js
+++ ninja/ninja.js
@@ -1,33 +1,93 @@
 // Sprite image downloaded free from: https://www.gameart2d.com/ninja-adventure---free-sprites.html
 
-function loadGame(){ // Initiates the game when the window loads
+//TODO turn image paths into variables so they only need to be declared once
+
+function gameMenu() {
+	var menu = document.createElement("div");
+	menu.id = "gameMenu";
+	menu.style.position = "relative";
+	menu.style.margin = "auto";
+	menu.style.top = "250px";
+	menu.style.height = "200px";
+	menu.style.width = "25%";
+	menu.style.backgroundColor = "blue";
+
+	var menuButton = document.createElement("button");
+	menuButton.id = "button";
+	menu.appendChild(menuButton);
+	menuButton.style.position = "absolute";
+	menuButton.innerHTML = "Play!";
+	menuButton.style.top = "45%";
+	menuButton.style.marginLeft = "45%";
+	menu.appendChild(menuButton);
+	document.body.appendChild(menu)
+
+	menuButton.onclick = function() {
+		menu.className = "fadeOut";
+		loadGame();
+		menuButton.onclick = "";
+	}
+}
+
+
+function loadGame(){
+
+	var width = window.innerWidth ;
+	var height = window.innerHeight -84;
+
+	var bounds = {x:width, y:height};
+
+	console.log(height);
+
+	console.log(window.innerHeight);
+
+	function CreateGameSpace() {
+
+		this.container = document.createElement("div");
+		this.container.id = "gameSpace";
+		this.container.style.position = "relative";
+		this.container.style.left = "15%";
+		this.container.style.marginLeft = "auto";
+		this.container.style.marginTop = "-200px";
+		this.container.style.height = bounds.y + "px";
+		this.container.style.width = bounds.x + "px";
+		document.body.appendChild(this.container);
+	}
+
+	console.log("div: " + document.getElementById("gameSpace"));
+
 
 	function Player() { // Creates a div to contain the sprite image and enables the movement and animation functions
 
+		// Objects containing the dimensions of the individual sprite actions. Used to set the image slicer during sprite animation
+		this.runningDims = {x:181.5, y:229};
+		this.climbDims = {x:141, y:232};
+		this.glidingDims = {x:221.5, y:227};
+		this.attackDims = {x:268, y:247.5};
+
 		this.div = null;
-		this.pos = {x:50, y:75}; // Sets the initial position of the div containing the sprite
+		this.pos = {x:50, y:75, a:(bounds.x - this.runningDims.x -50), b:(bounds.y - this.runningDims.y -75)}; // Sets the initial position of the div containing the sprite
 		var dir = {x:1, y:0}; // Sets the initial direction of the sprite (facing right)
 
-		// Objects containing the dimensions of the individual sprite actions. Used to set the image slicer during sprite animation
-		this.runningDims = {x:363, y:458};
-		this.climbDims = {x:282, y:464};
-		this.glidingDims = {x:443, y:454};
-		this.attackDims = {x:536, y:495};
+
 
 		// Creates the div that will contain the sprite
 		// Logic based on Create function from Space Invaders game written by Benjamin Kenwright: https://f28wp.github.io/material/games/Game%2001%20-%20Internet%20and%20Web.html
 		this.Create = function() {
 			this.div = document.createElement("div");
 			this.div.id = "ninja";
-			// this.div.className = "flashing"; - Class to be added in a later iteration
-			this.div.style.position = "absolute";
+			 //- Class to be added in a later iteration
+			this.div.style.position = "relative";
 			this.div.style.left = this.pos.x + "px";
 			this.div.style.top 	= this.pos.y + "px";
 			this.div.style.height = this.runningDims.y + "px";
 			this.div.style.width = this.runningDims.x + "px";
-			this.div.style.background = "url(../ninja/sprites/ninjaRunRight.png)";
+            this.div.style.background = "url(../ninja/sprites/ninjaRunRight.png)";
+			this.div.style.backgroundSize = "cover";
+			// this.div.style.border = "2px solid black"
 			this.div.style.padding = "0pt";
-			document.body.appendChild( this.div );
+            // gameSpace.appendChild( this.div );
+            document.getElementById("gameSpace").appendChild(this.div);
 		}
 
 		// Function that moves the div around the screen depending on the key that is pressed. Increments the positioning coordinates based on the set speed
@@ -39,18 +99,22 @@ function loadGame(){ // Initiates the game when the window loads
 			if (String.fromCharCode(i.keyCode) == "D") {
 				dir.x = 1;
 				this.pos.x += dir.x * speed;
+				this.pos.a -= dir.x * speed;
 			}
 			if (String.fromCharCode(i.keyCode) == "A") {
 				dir.x = -1;
 				this.pos.x += dir.x * speed;
+				this.pos.a -= dir.x * speed;
 			}
 			if (String.fromCharCode(i.keyCode) == "W") {
 				dir.y = -1;
 				this.pos.y += dir.y * speed;
+				this.pos.b -= dir.y * speed;
 			}
 			if (String.fromCharCode(i.keyCode) == "S") {
 				dir.y = 1;
 				this.pos.y += dir.y * speed;
+				this.pos.b -= dir.y * speed;
 			}
 
 			// Ensures the div does not move off the page by setting a limit on its lateral and vertical movement
@@ -63,14 +127,14 @@ function loadGame(){ // Initiates the game when the window loads
 			if (this.pos.x < borderX) {
 				dx = this.pos.x - borderX;
 			}
-			if (this.pos.x > (document.body.clientWidth - this.runningDims.x - borderX)) {
-				dx = this.pos.x - (document.body.clientWidth - this.runningDims.x - borderX);
+			if (this.pos.x > (bounds.x - this.runningDims.x - borderX)) {
+				dx = this.pos.x - (bounds.x - this.runningDims.x - borderX);
 			}
 			if (this.pos.y < borderY) {
 				dy = this.pos.y - borderY;
 			}
-			if (this.pos.y > (height- this.runningDims.y - borderY)) {
-				dy = this.pos.y - (height - this.runningDims.y - borderY);
+			if (this.pos.y > (bounds.y- this.runningDims.y - borderY)) {
+				dy = this.pos.y - (bounds.y - this.runningDims.y - borderY);
 			}
 			this.pos.x -= dx;
 			this.pos.y -= dy;
@@ -78,6 +142,8 @@ function loadGame(){ // Initiates the game when the window loads
 			// Sets the new position of the div
 			this.div.style.left 	= this.pos.x + "px";
 			this.div.style.top      = this.pos.y + "px";
+			console.log(this.div.style.right);
+			return this.pos;
 		}
 
 		// Function that animates the sprite within the div by using the backgroundPosition property as an image slicer over the sprite sheet 
@@ -86,17 +152,18 @@ function loadGame(){ // Initiates the game when the window loads
 		// If the D key is pressed, sprite animates to be running while facing to the right
 		if (String.fromCharCode(i.keyCode) == "D") {
 
-			this.div.style.height = this.runningDims.y + "px";
-			this.div.style.width = this.runningDims.x + "px";
+            this.div.style.background = "url(../ninja/sprites/ninjaRunRight.png)";
 
-			this.div.style.background = "url(../ninja/sprites/ninjaRunRight.png)";
-	
+			this.div.style.height = this.runningDims.y + "px";
+            this.div.style.width = this.runningDims.x + "px";
+            document.getElementById("ninja").style.backgroundSize = "cover";
+
 			var position = 363;
 			const diff = 363;
 	
 			interval = setInterval(function() {
 				
-				document.getElementById("ninja").style.backgroundPosition = position + "px 0px";
+                document.getElementById("ninja").style.backgroundPosition = position + "px 0px";
 	
 				if (position < 3630) {
 					position = position - diff;
@@ -113,10 +180,12 @@ function loadGame(){ // Initiates the game when the window loads
 		// If A key is pressed, sprite animates to be running while facing to the left
 		if (String.fromCharCode(i.keyCode) == "A") {
 
+            this.div.style.background = "url(../ninja/sprites/ninjaRunLeft.png)";
+
 			this.div.style.height = this.runningDims.y + "px";
 			this.div.style.width = this.runningDims.x + "px";
 
-			this.div.style.background = "url(../ninja/sprites/ninjaRunLeft.png)";
+            document.getElementById("ninja").style.backgroundSize = "cover";
 	
 			var position = 363;
 			const diff = 363;
@@ -137,10 +206,12 @@ function loadGame(){ // Initiates the game when the window loads
 		// If the W key is pressed, the sprite animates to be climbing vertically up the page
 		if (String.fromCharCode(i.keyCode) == "W") {
 
+            this.div.style.background = "url(../ninja/sprites/ninjaClimb.png)";
+
 			this.div.style.height = this.climbDims.y + "px";
 			this.div.style.width = this.climbDims.x + "px";
-
-			this.div.style.background = "url(../ninja/sprites/ninjaClimb.png)";
+           
+            document.getElementById("ninja").style.backgroundSize = "cover";
 	
 			var position = 282;
 			const diff = 282;
@@ -171,7 +242,8 @@ function loadGame(){ // Initiates the game when the window loads
 			}
 			else if (dir.x == -1) {
 				this.div.style.background = "url(../ninja/sprites/glideLeft.png)";
-			}
+            }
+            document.getElementById("ninja").style.backgroundSize = "cover";
 	
 			var position = 443;
 			const diff = 443;
@@ -200,8 +272,9 @@ function loadGame(){ // Initiates the game when the window loads
 			}
 			else if (dir.x == -1) {
 				this.div.style.background = "url(../ninja/sprites/ninjaFlip.png)";
-			}
-	
+            }
+            document.getElementById("ninja").style.backgroundSize = "cover";
+            
 			var position = 536;
 			const diff = 536;
 	
@@ -220,12 +293,79 @@ function loadGame(){ // Initiates the game when the window loads
 	}
  
 	}
+
+	function Enemy() {
+
+		var size = {x:215.4, y:259.5};
+		var position = {x:950, y:75, a:(bounds.x - size.x - 950) , b:(bounds.y - size.y -80) };
+
+		this.CreateEnemy = function() {
+			this.enemy 				    = document.createElement("div");
+			this.enemy.id 			    = "enemy";
+			this.enemy.style.position   = "absolute";
+			this.enemy.style.height     = size.y + "px";
+			this.enemy.style.width      = size.x + "px";
+			this.enemy.style.top        = position.y + "px";
+			this.enemy.style.left      = position.x + "px";
+			this.enemy.style.background = "url(../ninja/sprites/zombieWalkLeft.png)";
+			this.enemy.style.backgroundSize   = "cover";
+			// this.enemy.style.border = "2px solid black"
+			document.getElementById("gameSpace").appendChild(this.enemy);
+			return position;
+		}
+
+		this.animateEnemy = function () {
+	
+			this.enemy.style.backgroundSize = "cover";
+
+			var position = 215.4;
+			var diff = 215.4;
+	
+			interval = setInterval(function() {			
+				document.getElementById("enemy").style.backgroundPosition = position + "px 0px";
+	
+				if (position < 2154) {
+					position = position - diff;
+				}
+				else {
+					position = 215.4;
+				}
+			}, 50);
+		}
+	}
+
+	CreateGameSpace();
+
+	var playerPos;
+	var enemyPos;
+
 	var ninja = new Player();
 	ninja.Create();
-	
+
+	var enemy = new Enemy();
+	enemyPos = enemy.CreateEnemy();
+	enemy.animateEnemy();
+
+	function Collision() {
+		document.getElementById("enemy").className = "fadeOutEnemy";
+	}
+
 	function keyDown(i) {
-		ninja.movePlayer(i);
+		playerPos = ninja.movePlayer(i);
+		console.log("Player position x: " + playerPos.x);
+		console.log("Enemy position x: " + enemyPos.x);
+		console.log("Player position a: " + playerPos.a);
+		console.log("Enemy position a: " + enemyPos.a);
+		console.log("Player position y: " + playerPos.y);
+		console.log("Enemy position y: " + enemyPos.y);
+		console.log("Player position b: " + playerPos.b);
+		console.log("Enemy position b: " + enemyPos.b);
+		if((playerPos.x +185.5) > enemyPos.x && playerPos.y <= (enemyPos.y + 20)
+				&& playerPos.b >= (enemyPos.b - 50) && String.fromCharCode(i.keyCode) == " "){
+			Collision();
+		}
 		ninja.animatePlayer(i);
+		return playerPos;
 	}
 
 	function keyUp(i) {
@@ -238,4 +378,6 @@ function loadGame(){ // Initiates the game when the window loads
 	
 }
 	
-window.onload = loadGame;
+window.onload = gameMenu;
+
+
