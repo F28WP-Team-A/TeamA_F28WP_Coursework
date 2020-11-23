441104721aaf458d34a32ea9f0141ba8efb16082
LiamHorrobin <70898897+LiamHorrobin@users.noreply.github.com>
2020-11-23 15:56:19 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,183 @@
+
 var express = require("express");
 var app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/Player/Index.html");
+var serv = require("http").Server(app);
+var io = require("socket.io")(serv, {});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/Client/index.html");
 });
+app.use("/Client", express.static(__dirname + "/Client"));
 
-app.use("/Player", express.static(__dirname + "/Player"));
+serv.listen(8080);
+console.log("Server started.");
 
-server.listen(8080);
-console.log("server started");
-var Players = {};
+var SOCKET_LIST = {};
+// creats a variable of enitiy
+// almost everything can be defined as an entity
+// has a position x and y on the canvas
+// has a speed x and speed Y
+// and has a id which is declared for each other variable
+var Entity = function () {
+  var self = {
+    x: 250,
+    y: 250,
+    spdX: 0,
+    spdY: 0,
+    id: ""
+  };
+  self.update = function () {
+    self.updatePosition();
+  };
+  self.updatePosition = function () {
+    self.x += self.spdX;
+    self.y += self.spdY;
+  };
+  self.getDistance = function (pt) {
+    return Math.sqrt(Math.pow(self.x - pt.x, 2) + Math.pow(self.y - pt.y, 2));
+  };
+  return self;
+};
 
-io.on("connection", function (socket) {
-  socket.on("new player", function () {
-    Players[socket.id] = {
-      x: 300,
-      y: 300
-    };
+var Player = function (id) {
+  var self = Entity();
+  self.id = id;
+  self.number = "" + Math.floor(10 * Math.random());
+  self.pressingRight = false;
+  self.pressingLeft = false;
+  self.pressingUp = false;
+  self.pressingDown = false;
+  self.pressingAttack = false;
+  self.mouseAngle = 0;
+  self.maxSpd = 10;
+  self.hp = 10;
+  self.maxHp = 10;
+  self.score = 10;
+
+  var super_update = self.update;
+  self.update = function () {
+    self.updateSpd();
+    super_update();
+
+    if (self.pressingAttack) {
+      self.shootBullet(self.mouseAngle);
+    }
+  };
+
+  self.shootBullet = function (angle) {
+    var b = Bullet(self.id, angle);
+    b.x = self.x;
+    b.y = self.y;
+  };
+
+  self.updateSpd = function () {
+    if (self.pressingRight) self.spdX = self.maxSpd;
+    else if (self.pressingLeft) self.spdX = -self.maxSpd;
+    else self.spdX = 0;
+
+    if (self.pressingUp) self.spdY = -self.maxSpd;
+    else if (self.pressingDown) self.spdY = self.maxSpd;
+    else self.spdY = 0;
+  };
+  Player.list[id] = self;
+  return self;
+};
+
+Player.list = {};
+
+Player.onConnect = function (socket) {
+  var player = Player(socket.id);
+  socket.on("keyPress", function (data) {
+    if (data.inputId === "left") player.pressingLeft = data.state;
+    else if (data.inputId === "right") player.pressingRight = data.state;
+    else if (data.inputId === "up") player.pressingUp = data.state;
+    else if (data.inputId === "down") player.pressingDown = data.state;
+    else if (data.inputId === "attack") player.pressingAttack = data.state;
+    else if (data.inputId === "mouseAngle") player.mouseAngle = data.state;
+  });
+};
+Player.onDisconnect = function (socket) {
+  delete Player.list[socket.id];
+};
+
+Player.update = function () {
+  var pack = [];
+  for (var i in Player.list) {
+    var player = Player.list[i];
+    player.update();
+    pack.push({
+      x: player.x,
+      y: player.y,
+      number: player.number,
+      hp: player.hp,
+      maxHp: player.maxHp,
+      score: player.score
+    });
+  }
+  return pack;
+};
+
+var Bullet = function (parent, angle) {
+  var self = Entity();
+  self.id = Math.random();
+  self.spdX = Math.cos((angle / 180) * Math.PI) * 10;
+  self.spdY = Math.sin((angle / 180) * Math.PI) * 10;
+
+  self.parent = parent;
+
+  self.timer = 0;
+  self.toRemove = false;
+  var super_update = self.update;
+  self.update = function () {
+    if (self.timer++ > 30) self.toRemove = true;
+    super_update();
+
+    for (var i in Player.list) {
+      var p = Player.list[i];
+      if (self.getDistance(p) < 32 && self.parent !== p.id) {
+        self.toRemove = true;
+      }
+    }
+  };
+  Bullet.list[self.id] = self;
+  return self;
+};
+Bullet.list = {};
+
+Bullet.update = function () {
+  var pack = [];
+  for (var i in Bullet.list) {
+    var bullet = Bullet.list[i];
+    bullet.update();
+    if (bullet.toRemove) delete Bullet.list[i];
+    else
+      pack.push({
+        x: bullet.x,
+        y: bullet.y
+      });
+  }
+  return pack;
+};
+
+io.sockets.on("connection", function (socket) {
+  socket.id = Math.random();
+  SOCKET_LIST[socket.id] = socket;
+
+  Player.onConnect(socket);
+
+  socket.on("disconnect", function () {
+    delete SOCKET_LIST[socket.id];
+    Player.onDisconnect(socket);
   });
 });
 
 setInterval(function () {
-  io.sockets.emit("state", Players);
-}, 1000 / 60);
+  var pack = {
+    player: Player.update(),
+    bullet: Bullet.update()
+  };
+
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.emit("newPositions", pack);
+  }
+}, 1000 / 25);
