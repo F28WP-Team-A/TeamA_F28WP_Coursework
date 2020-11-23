ab50acf3c6b579b46212ac5491478e009cb7ea2d
melucas2000 <72085283+melucas2000@users.noreply.github.com>
2020-11-23 18:54:06 +0000

    added file

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+
+var io = require("socket.io")(http);
+
+var players = [];
+var coins = [];
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/login/login.html");
+});
+
+app.get("/play", (req, res) => {
+  res.sendFile(__dirname + "/public/game/index.html");
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
