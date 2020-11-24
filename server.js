9a94fa53090d7bc330f809db85fa632e29a1e529
melucas2000 <72085283+melucas2000@users.noreply.github.com>
2020-11-24 21:03:04 +0000

    change file paths

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var http = require("http").createServer(app);
 
 var io = require("socket.io")(http);
 
-var players = [];
-var coins = [];
+/* linking the public folder */
 app.use(express.static(__dirname + "/public"));
 
+/* linking the login html page */
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login/login.html");
 });
 
-/* change the file path to the correspoding game play one */
+/* linking the game state html page */
 app.get("/play", (req, res) => {
-  res.sendFile(__dirname + "/public/game/index.html");
+  res.sendFile(__dirname + "/client/index.html");
 });
 
+/* login page hosted on local host 3000 */
 http.listen(3000, () => {
   console.log("listening on *:3000");
 });
