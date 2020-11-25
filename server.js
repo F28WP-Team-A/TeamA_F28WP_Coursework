171fc339f12c3dad096a871f9d87ccd8c34e048f
melucas2000 <72085283+melucas2000@users.noreply.github.com>
2020-11-25 17:17:22 +0000

    database login

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var path = require("path");
 var app = express();
 var http = require("http").createServer(app);
 
 var io = require("socket.io")(http);
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Melstar1",
+  database: "nodelogin",
+});
+
+app.use(
+  session({
+    secret: "sec",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 /* linking the public folder */
 app.use(express.static(__dirname + "/public"));
@@ -12,9 +34,48 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/login/login.html");
 });
 
-/* linking the game state html page */
+app.post("/auth", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    connection.connect();
+
+    connection.query(
+      "SELECT * FROM accounts WHERE email = ? AND password = ?",
+      [email, password],
+      function (error, results, fields) {
+        console.log(results);
+        if (results && results.length > 0) {
+          req.session.loggedin = true;
+          req.session.email = email;
+          res.redirect("/play");
+        } else {
+          res.send("Incorrect email or password.");
+        }
+        res.end();
+      }
+    );
+
+    connection.end();
+  } else {
+    res.send("Enter email and password.");
+    res.end();
+  }
+});
+
+// /* linking the game state html page */
+// app.get("/play", (req, res) => {
+//   res.sendFile(__dirname + "/client/index.html");
+// });
+
 app.get("/play", (req, res) => {
-  res.sendFile(__dirname + "/client/index.html");
+  if (req.session.loggedin) {
+    res.send("Welcome " + req.session.email);
+  } else {
+    res.send("Please login");
+  }
+  res.end();
 });
 
 /* login page hosted on local host 3000 */
