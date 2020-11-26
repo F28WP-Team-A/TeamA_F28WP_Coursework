a52faf9f1d15434ba8f4f9aa71e085cfdc004d4e
melucas2000 <72085283+melucas2000@users.noreply.github.com>
2020-11-26 11:05:22 +0000

    server

---
+++ server.js
@@ -0,0 +1,86 @@
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var path = require("path");
+var app = express();
+var http = require("http").createServer(app);
+
+var io = require("socket.io")(http);
+
+/* database connection */
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodelogin",
+});
+
+app.use(
+  session({
+    secret: "sec",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* linking the public folder */
+app.use(express.static(__dirname + "/public"));
+
+/* linking the login html page */
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/login/login.html");
+});
+
+/* post request */
+app.post("/auth", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    connection.connect();
+
+    /* query database */
+    connection.query(
+      "SELECT * FROM accounts WHERE email = ? AND password = ?",
+      [email, password],
+      function (error, results, fields) {
+        console.log(results);
+        if (results && results.length > 0) {
+          req.session.loggedin = true;
+          req.session.email = email;
+          res.redirect("/play");
+        } else {
+          res.send("Incorrect email or password.");
+        }
+        res.end();
+      }
+    );
+
+    connection.end();
+  } else {
+    res.send("Enter email and password.");
+    res.end();
+  }
+});
+
+/* promt login success message */
+app.get("/play", (req, res) => {
+  if (req.session.loggedin) {
+    res.send("Welcome " + req.session.email);
+  } else {
+    /* linking the game state html page */
+    app.get("/play", (req, res) => {
+      res.sendFile(__dirname + "/client/index.html");
+    });
+  }
+  res.end();
+});
+
+/* login page hosted on local host 3000 */
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
